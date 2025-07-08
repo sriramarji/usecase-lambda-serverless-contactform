c108442eb7ea7b4c9ae57c80775c79b223c200df
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-07-08 18:26:15 +0530

    modified syntax issues for tf files

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "lambda" {
   source                = "./modules/lambda"
 
-  lambda_function_name  =  var.my-function_name
+  function_name  =  var.my-function_name
   handler =  var.my-handler
   runtime =  var.my-runtime
   environment_vars = var.my-environment_variables
-  aws_apigatewayv2_arn = var.my-lambda_role_name
+  #aws_apigatewayv2_arn = var.my-lambda_role_name
   lambda_zip_path = var.my-lambda_zip_path
 
 }
