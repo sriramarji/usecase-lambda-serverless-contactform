537e9387e5132f6527cf759190e42187d27f0b00
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-07-09 19:03:25 +0530

    modified changes

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">= 1.12.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.2.0"
     }
   }
-  required_version = ">= 1.12.0"
 }
 
 provider "aws" {
